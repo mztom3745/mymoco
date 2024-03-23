42925dc7f5e0100f1a07464e902d8fdd069ec839
mztom3745 <103809026+mztom3745@users.noreply.github.com>
2024-03-23 18:34:32 +0800

    Update builder.py

--- moco/builder.py
+++ moco/builder.py
@@ -31,7 +31,7 @@ class MoCo(nn.Module):
         self.encoder_q = base_encoder(num_classes=dim)
         self.encoder_k = base_encoder(num_classes=dim)
 
-        if mlp:  # hack: brute-force replacement 添加了一个线性层
+        if mlp:  # hack: brute-force replacement 添加了一个mlp
             dim_mlp = self.encoder_q.fc.weight.shape[1]
             self.encoder_q.fc = nn.Sequential(
                 nn.Linear(dim_mlp, dim_mlp), nn.ReLU(), self.encoder_q.fc

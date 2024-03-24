c59483415275db184bbea6a122c379acd5391449
mztom3745 <103809026+mztom3745@users.noreply.github.com>
2024-03-24 14:47:10 +0800

    Update main_lincls.py

--- main_lincls.py
+++ main_lincls.py
@@ -454,8 +454,8 @@ def train(train_loader, model, criterion, optimizer, epoch, args):
         # measure accuracy and record loss
         acc1, acc5 = accuracy(output, target, topk=(1, 5))
         losses.update(loss.item(), images.size(0))
-        
-        
+        top1.update(acc1[0], images.size(0))
+        top5.update(acc5[0], images.size(0))
 
         # compute gradient and do SGD step
         optimizer.zero_grad()

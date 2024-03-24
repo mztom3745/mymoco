039283e3f1f3a28276a4dfc6a0234b71ade8533a
mztom3745 <103809026+mztom3745@users.noreply.github.com>
2024-03-24 14:12:13 +0800

    Update main_fewshot.py

--- main_fewshot.py
+++ main_fewshot.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 
 # This source code is licensed under the MIT license found in the
@@ -8,15 +7,12 @@
 
 import argparse
 import builtins
-import math
 import os
 import random
 import shutil
 import time
 import warnings
 
-import moco.builder
-import moco.loader
 import torch
 import torch.backends.cudnn as cudnn
 import torch.distributed as dist
@@ -56,7 +52,7 @@ parser.add_argument(
     help="number of data loading workers (default: 32)",
 )
 parser.add_argument(
-    "--epochs", default=200, type=int, metavar="N", help="number of total epochs to run"
+    "--epochs", default=100, type=int, metavar="N", help="number of total epochs to run"
 )
 parser.add_argument(
     "--start-epoch",
@@ -78,7 +74,7 @@ parser.add_argument(
 parser.add_argument(
     "--lr",
     "--learning-rate",
-    default=0.03,
+    default=30.0,
     type=float,
     metavar="LR",
     help="initial learning rate",
@@ -86,26 +82,24 @@ parser.add_argument(
 )
 parser.add_argument(
     "--schedule",
-    default=[120, 160],
+    default=[60, 80],
     nargs="*",
     type=int,
-    help="learning rate schedule (when to drop lr by 10x)",
+    help="learning rate schedule (when to drop lr by a ratio)",
 )
-parser.add_argument(
-    "--momentum", default=0.9, type=float, metavar="M", help="momentum of SGD solver"
-)
+parser.add_argument("--momentum", default=0.9, type=float, metavar="M", help="momentum")
 parser.add_argument(
     "--wd",
     "--weight-decay",
-    default=1e-4,
+    default=0.0,
     type=float,
     metavar="W",
-    help="weight decay (default: 1e-4)",
+    help="weight decay (default: 0.)",
     dest="weight_decay",
 )
 parser.add_argument(
     "-p",
-    "--print-freq",#每十个batchsize打印一次
+    "--print-freq",
     default=10,
     type=int,
     metavar="N",
@@ -117,6 +111,13 @@ parser.add_argument(
     type=str,
     metavar="PATH",
     help="path to latest checkpoint (default: none)",
+)
+parser.add_argument(
+    "-e",
+    "--evaluate",
+    dest="evaluate",
+    action="store_true",
+    help="evaluate model on validation set",
 )
 parser.add_argument(
     "--world-size",
@@ -149,32 +150,11 @@ parser.add_argument(
     "multi node data parallel training",
 )
 
-# moco specific configs:
 parser.add_argument(
-    "--moco-dim", default=128, type=int, help="feature dimension (default: 128)"
-)
-parser.add_argument(
-    "--moco-k",
-    default=65536,
-    type=int,
-    help="queue size; number of negative keys (default: 65536)",
-)
-parser.add_argument(
-    "--moco-m",
-    default=0.999,
-    type=float,
-    help="moco momentum of updating key encoder (default: 0.999)",
-)
-parser.add_argument(
-    "--moco-t", default=0.07, type=float, help="softmax temperature (default: 0.07)"
+    "--pretrained", default="", type=str, help="path to moco pretrained checkpoint"
 )
 
-# options for moco v2
-parser.add_argument("--mlp", action="store_true", help="use mlp head")
-parser.add_argument(
-    "--aug-plus", action="store_true", help="use moco v2 data augmentation"
-)
-parser.add_argument("--cos", action="store_true", help="use cosine lr schedule")
+best_acc1 = 0
 
 
 def main():
@@ -217,6 +197,7 @@ def main():
 
 
 def main_worker(gpu, ngpus_per_node, args):
+    global best_acc1
     args.gpu = gpu
 
     # suppress printing if not master
@@ -233,11 +214,9 @@ def main_worker(gpu, ngpus_per_node, args):
     if args.distributed:
         if args.dist_url == "env://" and args.rank == -1:
             args.rank = int(os.environ["RANK"])
-            # 每个进程在启动时都会从环境变量中获取自己的排名
         if args.multiprocessing_distributed:
             # For multiprocessing distributed training, rank needs to be the
             # global rank among all the processes
-            # 转换为全局排名
             args.rank = args.rank * ngpus_per_node + gpu
         dist.init_process_group(
             backend=args.dist_backend,
@@ -247,16 +226,45 @@ def main_worker(gpu, ngpus_per_node, args):
         )
     # create model
     print("=> creating model '{}'".format(args.arch))
-    model = moco.builder.MoCo(
-        models.__dict__[args.arch],
-        args.moco_dim,
-        args.moco_k,
-        args.moco_m,
-        args.moco_t,
-        args.mlp,
-    )
-    print(model)
+    model = models.__dict__[args.arch]()#只是创建了一个resnet50
+    #替换1000维输出为分类维度
+    fc_inputs = model.fc.in_features
+    model.fc = nn.Linear(fc_inputs, 9)
+    print(model) #added
+    
+    # freeze all layers but the last fc
+    #for name, param in model.named_parameters():
+    #    if name not in ["fc.weight", "fc.bias"]:
+    #        param.requires_grad = False
+    # init the fc layer
+    model.fc.weight.data.normal_(mean=0.0, std=0.01)
+    model.fc.bias.data.zero_()
 
+    # load from pre-trained, before DistributedDataParallel constructor
+    if args.pretrained:
+        if os.path.isfile(args.pretrained):
+            print("=> loading checkpoint '{}'".format(args.pretrained))
+            checkpoint = torch.load(args.pretrained, map_location="cpu")
+
+            # rename moco pre-trained keys
+            state_dict = checkpoint["state_dict"]
+            for k in list(state_dict.keys()):
+                if k.startswith("module.encoder_q") and not k.startswith(
+                    "module.encoder_q.fc"
+                ):
+                # delete renamed or unused k
+                del state_dict[k]
+
+            args.start_epoch = 0
+            msg = model.load_state_dict(state_dict, strict=False)
+            print("missing_keys",set(msg.missing_keys))
+            assert set(msg.missing_keys) == {"fc.weight", "fc.bias"}# 不加载最后的fc层
+
+            print("=> loaded pre-trained model '{}'".format(args.pretrained))
+        else:
+            print("=> no checkpoint found at '{}'".format(args.pretrained))
+   
+    
     if args.distributed:
         # For multiprocessing distributed, DistributedDataParallel constructor
         # should always set the single device scope, otherwise,
@@ -280,21 +288,25 @@ def main_worker(gpu, ngpus_per_node, args):
     elif args.gpu is not None:
         torch.cuda.set_device(args.gpu)
         model = model.cuda(args.gpu)
-        # comment out the following line for debugging
-        raise NotImplementedError("Only DistributedDataParallel is supported.")
     else:
-        # AllGather implementation (batch shuffle, queue update, etc.) in
-        # this code only supports DistributedDataParallel.
-        raise NotImplementedError("Only DistributedDataParallel is supported.")
+        # DataParallel will divide and allocate batch_size to all available GPUs
+        if args.arch.startswith("alexnet") or args.arch.startswith("vgg"):
+            model.features = torch.nn.DataParallel(model.features)
+            model.cuda()
+        else:
+            model = torch.nn.DataParallel(model).cuda()
 
     # define loss function (criterion) and optimizer
     criterion = nn.CrossEntropyLoss().cuda(args.gpu)
 
+    # optimize only the linear classifier 
+    '''
+    parameters = list(filter(lambda p: p.requires_grad, model.parameters()))
+    assert len(parameters) == 2  # fc.weight, fc.bias
+    '''
+    
     optimizer = torch.optim.SGD(
-        model.parameters(),
-        args.lr,
-        momentum=args.momentum, # 非moco动量
-        weight_decay=args.weight_decay,
+        parameters, args.lr, momentum=args.momentum, weight_decay=args.weight_decay
     )
 
     # optionally resume from a checkpoint
@@ -308,6 +320,10 @@ def main_worker(gpu, ngpus_per_node, args):
                 loc = "cuda:{}".format(args.gpu)
                 checkpoint = torch.load(args.resume, map_location=loc)
             args.start_epoch = checkpoint["epoch"]
+            best_acc1 = checkpoint["best_acc1"]
+            if args.gpu is not None:
+                # best_acc1 may be from a checkpoint from a different GPU
+                best_acc1 = best_acc1.to(args.gpu)
             model.load_state_dict(checkpoint["state_dict"])
             optimizer.load_state_dict(checkpoint["optimizer"])
             print(
@@ -322,35 +338,21 @@ def main_worker(gpu, ngpus_per_node, args):
 
     # Data loading code
     traindir = os.path.join(args.data, "train")
+    valdir = os.path.join(args.data, "val")
     normalize = transforms.Normalize(
         mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
-    )# 用于对图像进行标准化处理，具体来说，就是将图像的像素值缩放到一个固定的范围
-    if args.aug_plus:
-        # MoCo v2's aug: similar to SimCLR https://arxiv.org/abs/2002.05709
-        augmentation = [
-            transforms.RandomResizedCrop(224, scale=(0.2, 1.0)),
-            transforms.RandomApply(
-                [transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8  # not strengthened
-            ),
-            transforms.RandomGrayscale(p=0.2),
-            transforms.RandomApply([moco.loader.GaussianBlur([0.1, 2.0])], p=0.5),
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-            normalize,
-        ]
-    else:
-        # MoCo v1's aug: the same as InstDisc https://arxiv.org/abs/1805.01978
-        augmentation = [
-            transforms.RandomResizedCrop(224, scale=(0.2, 1.0)),
-            transforms.RandomGrayscale(p=0.2),
-            transforms.ColorJitter(0.4, 0.4, 0.4, 0.4),
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-            normalize,
-        ]
+    )
 
     train_dataset = datasets.ImageFolder(
-        traindir, moco.loader.TwoCropsTransform(transforms.Compose(augmentation))# 文件夹路径和指定数据增强方法
+        traindir,
+        transforms.Compose(
+            [
+                transforms.RandomResizedCrop(224),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                normalize,
+            ]
+        ),
     )
 
     if args.distributed:
@@ -361,37 +363,64 @@ def main_worker(gpu, ngpus_per_node, args):
     train_loader = torch.utils.data.DataLoader(
         train_dataset,
         batch_size=args.batch_size,
-        shuffle=(train_sampler is None),# 即false，因为有train_sampler
+        shuffle=(train_sampler is None),
         num_workers=args.workers,
-        pin_memory=True, # 数据加载器将把数据缓存在CPU的内存中，而不是使用系统内存
-        sampler=train_sampler,# train_sampler已经实现了自己的采样策略
-        drop_last=True,
+        pin_memory=True,
+        sampler=train_sampler,
     )
+
+    val_loader = torch.utils.data.DataLoader(
+        datasets.ImageFolder(
+            valdir,
+            transforms.Compose(
+                [
+                    transforms.Resize(256),
+                    transforms.CenterCrop(224),
+                    transforms.ToTensor(),
+                    normalize,
+                ]
+            ),
+        ),
+        batch_size=args.batch_size,
+        shuffle=False,
+        num_workers=args.workers,
+        pin_memory=True,
+    )
+
+    if args.evaluate:
+        validate(val_loader, model, criterion, args)
+        return
 
     for epoch in range(args.start_epoch, args.epochs):
         if args.distributed:
             train_sampler.set_epoch(epoch)
-            # 为了让每一个epoch的采样顺序不一样，在每次epoch循环的开始，
-            # 需要调用set_epoch()函数来更新 self.epoch
         adjust_learning_rate(optimizer, epoch, args)
 
         # train for one epoch
         train(train_loader, model, criterion, optimizer, epoch, args)
 
+        # evaluate on validation set
+        acc1 = validate(val_loader, model, criterion, args)
+        
+        # remember best acc@1 and save checkpoint
+        is_best = acc1 > best_acc1
+        best_acc1 = max(acc1, best_acc1)
+
         if not args.multiprocessing_distributed or (
-            args.multiprocessing_distributed and args.rank % ngpus_per_node == 0 
-            # ngpus_per_node为gpu总数，即一轮训练完成后
+            args.multiprocessing_distributed and args.rank % ngpus_per_node == 0
         ):
             save_checkpoint(
                 {
                     "epoch": epoch + 1,
                     "arch": args.arch,
                     "state_dict": model.state_dict(),
+                    "best_acc1": best_acc1,
                     "optimizer": optimizer.state_dict(),
                 },
-                is_best=False,
-                filename="checkpoint_{:04d}.pth.tar".format(epoch),
+                is_best,
             )
+            if epoch == args.start_epoch:
+                #sanity_check(model.state_dict(), args.pretrained)#需要训练全部的权重
 
 
 def train(train_loader, model, criterion, optimizer, epoch, args):
@@ -406,28 +435,36 @@ def train(train_loader, model, criterion, optimizer, epoch, args):
         prefix="Epoch: [{}]".format(epoch),
     )
 
-    # switch to train mode
-    model.train()
-
+    """
+    Switch to eval mode:
+    Under the protocol of linear classification on frozen features/models,
+    it is not legitimate to change any part of the pre-trained model.
+    BatchNorm in train mode may revise running mean/std (even if it receives
+    no gradient), which are part of the model parameters too.
+    """
+    model.eval()
+    
     end = time.time()
-    for i, (images, _) in enumerate(train_loader): # 忽略数据集中的target，同时获取索引和元素
+    for i, (images, target) in enumerate(train_loader):
         # measure data loading time
         data_time.update(time.time() - end)
-
+        
         if args.gpu is not None:
-            images[0] = images[0].cuda(args.gpu, non_blocking=True)
-            images[1] = images[1].cuda(args.gpu, non_blocking=True)
-
+            images = images.cuda(args.gpu, non_blocking=True)
+            #print("*t**images'shape:",images.size())#added
+        target = target.cuda(args.gpu, non_blocking=True)
+        #print("target",target.size())
         # compute output
-        output, target = model(im_q=images[0], im_k=images[1])# Nx(1+K)
+        output = model(images)
+        #print("output",output.size())
+        #print("***")
         loss = criterion(output, target)
 
-        # acc1/acc5 are (K+1)-way contrast classifier accuracy
         # measure accuracy and record loss
         acc1, acc5 = accuracy(output, target, topk=(1, 5))
-        losses.update(loss.item(), images[0].size(0))
-        top1.update(acc1[0], images[0].size(0))
-        top5.update(acc5[0], images[0].size(0))
+        losses.update(loss.item(), images.size(0))
+        
+        
 
         # compute gradient and do SGD step
         optimizer.zero_grad()
@@ -439,7 +476,56 @@ def train(train_loader, model, criterion, optimizer, epoch, args):
         end = time.time()
 
         if i % args.print_freq == 0:
-            progress.display(i) #打印信息
+            progress.display(i)
+
+
+def validate(val_loader, model, criterion, args):
+    batch_time = AverageMeter("Time", ":6.3f")
+    losses = AverageMeter("Loss", ":.4e")
+    top1 = AverageMeter("Acc@1", ":6.2f")
+    top5 = AverageMeter("Acc@5", ":6.2f")
+    progress = ProgressMeter(
+        len(val_loader), [batch_time, losses, top1, top5], prefix="Test: "
+    )
+
+    # switch to evaluate mode
+    model.eval()
+
+    with torch.no_grad():
+        end = time.time()
+        for i, (images, target) in enumerate(val_loader):
+            if args.gpu is not None:
+                images = images.cuda(args.gpu, non_blocking=True)
+                #print("**v*image",images.size())
+            target = target.cuda(args.gpu, non_blocking=True)
+            #print("target:",target)
+                
+            # compute output
+            output = model(images)
+            _, pred = output.topk(5, 1, True, True)
+            #print("pred5 is",pred)
+            #print("output:",output.size())
+            loss = criterion(output, target)
+            #print("***")
+            
+            # measure accuracy and record loss
+            acc1, acc5 = accuracy(output, target, topk=(1, 5))
+            losses.update(loss.item(), images.size(0))
+            top1.update(acc1[0], images.size(0))
+            top5.update(acc5[0], images.size(0))
+
+            # measure elapsed time
+            batch_time.update(time.time() - end)
+            end = time.time()
+
+            if i % args.print_freq == 0:
+                progress.display(i)
+
+        # TODO: this should also be done with the ProgressMeter
+        print(
+            " * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}".format(top1=top1, top5=top5)
+        )
+    return top1.avg
 
 
 def save_checkpoint(state, is_best, filename="checkpoint.pth.tar"):
@@ -462,7 +548,7 @@ class AverageMeter:
         self.sum = 0
         self.count = 0
 
-    def update(self, val, n=1):# n为维度
+    def update(self, val, n=1):
         self.val = val
         self.sum += val * n
         self.count += n
@@ -486,23 +572,20 @@ class ProgressMeter:
 
     def _get_batch_fmtstr(self, num_batches):
         num_digits = len(str(num_batches // 1))
-        fmt = "{:" + str(num_digits) + "d}"# 字符串格式（即batches的格式
-        return "[" + fmt + "/" + fmt.format(num_batches) + "]"# 打印当前[批次/单epoch的总批次]
+        fmt = "{:" + str(num_digits) + "d}"
+        return "[" + fmt + "/" + fmt.format(num_batches) + "]"
 
 
 def adjust_learning_rate(optimizer, epoch, args):
     """Decay the learning rate based on schedule"""
     lr = args.lr
-    if args.cos:  # cosine lr schedule 余弦退火调度策略
-        lr *= 0.5 * (1.0 + math.cos(math.pi * epoch / args.epochs))
-    else:  # stepwise lr schedule 阶梯状调度策略
-        for milestone in args.schedule:
-            lr *= 0.1 if epoch >= milestone else 1.0
+    for milestone in args.schedule:
+        lr *= 0.1 if epoch >= milestone else 1.0
     for param_group in optimizer.param_groups:
         param_group["lr"] = lr
 
 
-def accuracy(output, target, topk=(1,)):# Nx(1+K)
+def accuracy(output, target, topk=(1,)):
     """Computes the accuracy over the k top predictions for the specified values of k"""
     with torch.no_grad():
         maxk = max(topk)
@@ -515,7 +598,6 @@ def accuracy(output, target, topk=(1,)):# Nx(1+K)
         res = []
         for k in topk:
             correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
-            # 将view(-1)改为reshape(-1)
             res.append(correct_k.mul_(100.0 / batch_size))
         return res
 

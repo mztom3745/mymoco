a0f6b8fe0747982c479fa78db1f26daeac82bbde
mztom3745 <103809026+mztom3745@users.noreply.github.com>
2024-03-26 12:09:46 +0800

    Update main_lincls_2.py

--- main_lincls_2.py
+++ main_lincls_2.py
@@ -344,7 +344,7 @@ def main_worker(gpu, ngpus_per_node, args):
 
     # Data loading code
     traindir = os.path.join(args.data, "train")
-    valdir = os.path.join(args.data, "val")
+    valdir = os.path.join(args.data, "test")#added:用测试集去看效果
     normalize = transforms.Normalize(
         mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
     )

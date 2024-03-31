6f959c3fa8dfeace2cd0c21175aab5796e2b3b62
mztom3745 <103809026+mztom3745@users.noreply.github.com>
2024-03-31 21:59:43 +0800

    Update main_moco_2.py

--- main_moco_2.py
+++ main_moco_2.py
@@ -438,6 +438,7 @@ def train(train_loader, model, criterion, optimizer, epoch, args):
         # acc1/acc5 are (K+1)-way contrast classifier accuracy
         # measure accuracy and record loss
         acc1, acc5 = accuracy(output, target, topk=(1, 5))
+        print("acc1.type:"acc1[0].type())
         losses.update(loss.item(), images[0].size(0))
         top1.update(acc1[0], images[0].size(0))
         top5.update(acc5[0], images[0].size(0))

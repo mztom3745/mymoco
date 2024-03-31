0f3775c490b12c4f21defe55c3dfa1ecb5025988
mztom3745 <103809026+mztom3745@users.noreply.github.com>
2024-03-31 13:22:13 +0800

    Update main_impash.py

--- main_impash.py
+++ main_impash.py
@@ -463,6 +463,9 @@ def train(train_loader, model, criterion, optimizer, epoch, args):
         top1b.update(acc1b[0], images[0].size(0))
         top1c.update(acc1c[0], images[0].size(0))
         top1d.update(acc1d[0], images[0].size(0))
+        print("testing")
+        print(acc1a[0])
+        print(top1a.avg)
 
         #added
         if args.train_accfile!="" and args.gpu == 0:

977eb15f33c2aadc91e1cb5e6280c949650cac5b
mztom3745 <103809026+mztom3745@users.noreply.github.com>
2024-04-01 15:10:26 +0800

    Update main_impash.py

--- main_impash.py
+++ main_impash.py
@@ -480,7 +480,7 @@ def train(train_loader, model, criterion, optimizer, epoch, args):
         #print("acc1a.item:",acc1a[0].item())
         #print("size:",images[0].size(0))
         #print(top1a)
-        print("over")
+        #print("over")
         #added
         if args.train_output!="" and args.gpu == 0 and i % args.print_freq == 0:
             with open(args.train_output,"a") as f:
